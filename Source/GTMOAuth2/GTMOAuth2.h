fa2b16247bcba15a0361a512d749e13cb0f9bbd4
Evan Coleman <e@edc.me>
2019-10-19 13:26:06 -0400

    add carthage support

---
+++ Source/GTMOAuth2/GTMOAuth2.h
@@ -0,0 +1,18 @@
+//
+//  GTMOAuth2.h
+//  GTMOAuth2
+//
+//  Created by Evan Coleman on 10/19/19.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GTMOAuth2.
+FOUNDATION_EXPORT double GTMOAuth2VersionNumber;
+
+//! Project version string for GTMOAuth2.
+FOUNDATION_EXPORT const unsigned char GTMOAuth2VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GTMOAuth2/PublicHeader.h>
+
+
